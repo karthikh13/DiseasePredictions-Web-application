cd0a545a9ad805433edf25128f466c94271d9286
karthikh13 <karthikhegde55146@gmail.com>
2025-07-23 23:15:33 +0530

    Fix: Stop overwriting runtime.txt in .render-build.sh and use Python 3.10.11

--- .render-build.sh
+++ .render-build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 echo "⚙️ Using custom build script"
 
-# Set Python version manually
-echo "python-3.10.12" > runtime.txt
-
 # Install dependencies manually
 pip install -r requirements.txt
